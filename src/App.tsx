6c86ef1ffe350418672879c24a14ee1976dfb4e9
Dmitry Brizhinev <abcdime@gmail.com>
2022-07-12 20:10:56 +1000

    Copied in existing code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,144 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react'
+import './App.css'
+import { User, getCurrentUserOrNull, loginPopup } from './auth'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export interface AppState {
+  user: User | null;
 }
 
-export default App;
+export default class App extends React.Component<object, AppState> {
+
+  constructor(props: any) {
+    super(props);
+
+    this.onClickLogin = this.onClickLogin.bind(this);
+
+    this.state = {user: getCurrentUserOrNull()};
+  }
+
+  async onClickLogin(event: React.FormEvent<HTMLButtonElement>) {
+    const user = await loginPopup();
+        /*}).catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });*/
+    this.setState({user: user});
+  }
+
+  render() {
+    if (this.state.user == null) {
+      return (
+        <main>
+        <h2>Recovery</h2>
+        <button onClick={this.onClickLogin}>
+          Login
+        </button>
+        <hr/>
+        </main>
+      );
+    } else {
+      return (
+        <main>
+        <h2>Recovery</h2>
+        User: {this.state.user.name}
+        <hr/>
+        <Page label="thoughts here"/>
+        <hr/>
+        <Page label="more here"/>
+        <hr/>
+        <Page label="thoughts"/>
+        <hr/>
+        <Page label="more"/>
+        <hr/>
+        </main>
+      );
+    }
+  }
+}
+
+const WIDTH = 100;
+const HEIGHT = 100;
+const SCALE = 4;
+
+export interface ArtProps {
+  seed: number;
+}
+
+function getX(t: number): number {
+  return Math.sin(t * Math.PI);
+}
+
+export class Art extends React.Component<ArtProps, object> {
+  lastKey:number;
+
+  constructor(props: ArtProps) {
+    super(props);
+
+    this.lastKey = 0;
+  }
+
+  MakeCircle(x: number, y:number, r:number, w:number): CircleProps {
+    this.lastKey += 1;
+    return {
+        x: x,
+        y: y,
+        r: r,
+        w: w,
+        key: this.lastKey,
+    };
+  }
+
+  render() {
+    return (
+      <Page label="thoughts here"/>
+    );
+  }
+}
+
+interface CircleProps {
+  x: number;
+  y: number;
+  r: number;
+  w: number;
+  key: number;
+}
+
+class Circle extends React.PureComponent<CircleProps, object> {
+  constructor(props: CircleProps) {
+    super(props);
+  }
+
+  render() {
+    return <circle cx={this.props.x} cy={this.props.y} r={this.props.r} strokeWidth={this.props.w} className={'golden'}/>;
+  }
+}
+
+interface PageProps {
+  label: string;
+}
+
+interface PageState {
+  text: string;
+}
+
+class Page extends React.Component<PageProps, PageState> {
+  constructor(props: PageProps) {
+    super(props);
+    this.state = {text: 'HELLO D'};
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event: React.FormEvent<HTMLTextAreaElement>) {
+    this.setState({text: event.target.value});
+  }
+
+  render() {
+    return (
+      <label>
+        {this.props.label}<br/>
+        <textarea value={this.state.text} onChange={this.handleChange} />
+      </label>
+    );
+  }
+}
---
+++ src/auth.tsx
@@ -0,0 +1,35 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider, signInWithRedirect, signInWithPopup } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDpeFI1YoAh9n1ibsczs60jU9MG3LbaIPE",
+  authDomain: "recovery-43b10.firebaseapp.com",
+  projectId: "recovery-43b10",
+  storageBucket: "recovery-43b10.appspot.com",
+  messagingSenderId: "844247155656",
+  appId: "1:844247155656:web:58ad57358a8a304620f6fd",
+  measurementId: "G-5PW6WKYCBF"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export interface User {
+  name: string;
+  id: string;
+}
+
+export function getCurrentUserOrNull(): User | null {
+  const user = auth.currentUser;
+  if (user == null) {
+    return null;
+  }
+  return {name: user.email, id: user.uid};
+}
+
+export async function loginPopup(): Promise<User> {
+  const provider = new GoogleAuthProvider();
+  const result = await signInWithPopup(auth, provider);
+  const user = result.user;
+  return {name: user.email, id: user.uid};
+}
